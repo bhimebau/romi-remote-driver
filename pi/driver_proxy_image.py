b5abbbdb9e67eba08b36227b734a599d09666880
Bryce Himebaugh <bhimebau@indiana.edu>
2024-04-16 22:36:46 +0100

    Pushed from romiu

--- pi/driver_proxy_image.py
+++ pi/driver_proxy_image.py
@@ -19,17 +19,21 @@ def toRedis(r,a,n,fnum):
 if __name__ == '__main__':
    baseboard = AStar()
    r = redis.Redis('localhost', port=6379, db=0, password='e101class')
+   r.set('motor_right', 0)
+   r.set('motor_left',0)
    cam = cv2.VideoCapture(0)
-   cam.set(3, 320)
-   cam.set(4, 240)
+   cam.set(3, 640)
+   cam.set(4, 480)
    count = 0
    led_val = 0;
    while True:
       ret, img = cam.read()
       toRedis(r, img, 'latest',count)
-      count += 1
       print(count)
-      baseboard.motors(80,80)
+      count += 1
+      ml = r.get("motor_left")
+      mr = r.get("motor_right")
+      baseboard.motors(int(ml),int(mr))
       if (led_val==0):
          led_val = 1
          baseboard.leds(0,0,led_val)
